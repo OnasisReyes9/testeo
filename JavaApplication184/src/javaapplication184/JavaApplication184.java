bed0b53e5a8c0ec97b1191ca93d653bb95864fdc
OnasisReyes9 <onasis660@unitec.com>
2020-08-02 20:07:30 -0600

    Factorial  2 2

--- JavaApplication184/src/javaapplication184/JavaApplication184.java
+++ JavaApplication184/src/javaapplication184/JavaApplication184.java
@@ -15,7 +15,21 @@ public class JavaApplication184 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println("Hola");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hola");
+        }
+        factorial(6);
     }
     
+    
+    public static int factorial(int n){
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
 }
